bd6bf890bd6aecd3a2f5683c371c2b14e11723b1
brand1313 <brand13@naver.com>
2022-08-24 03:41:35 +0900

    Edit IndexController to get current user information

--- src/main/java/com/brand13/bookstore/bookstoreproj/controller/IndexController.java
+++ src/main/java/com/brand13/bookstore/bookstoreproj/controller/IndexController.java
@@ -2,11 +2,20 @@ package com.brand13.bookstore.bookstoreproj.controller;
 
 import com.brand13.bookstore.bookstoreproj.controller.dto.PostsResponseDto;
 import com.brand13.bookstore.bookstoreproj.service.PostsService;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.IDToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 public class IndexController {
@@ -35,5 +44,28 @@ public class IndexController {
         model.addAttribute("post", dto);
 
         return "posts-update";
+    }
+
+    @GetMapping(path = "/users")
+    @ResponseBody
+    public String getUserInfo(Model model) {
+
+        KeycloakAuthenticationToken authentication =
+                (KeycloakAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+
+        Principal principal = (Principal) authentication.getPrincipal();
+
+        String userIdByToken = "";
+        String userName = "";
+
+        if (principal instanceof KeycloakPrincipal) {
+            KeycloakPrincipal<KeycloakSecurityContext> kPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) principal;
+            IDToken token = kPrincipal.getKeycloakSecurityContext().getIdToken();
+            userIdByToken = token.getSubject();
+            userName = token.getPreferredUsername();
+        }
+
+//        model.addAttribute("userIDByToken", userIdByToken);
+        return userName;
     }
 }
